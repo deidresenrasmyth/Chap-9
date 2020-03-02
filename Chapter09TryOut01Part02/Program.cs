609436932999fc6a06091210e1ef32acddcd9d1c
Deidre <deidre.balard@outlook.com>
2020-03-02 17:47:23 -0500

    commit

---
+++ Chapter09TryOut01Part02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chapter09TryOut02;
+
+namespace Chapter09TryOut01Part02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyExternalClass myObj = new MyExternalClass();
+            Console.WriteLine(myObj.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter09TryOut03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter09TryOut03
+{
+    class Program
+    {
+        class MyClass
+        {
+            public int val;
+        }
+        struct myStruct
+        {
+            public int val;
+        }
+        static void Main(string[] args)
+        {
+            MyClass objectA = new MyClass();
+            MyClass objectB = objectA;
+            objectA.val = 10;
+            objectB.val = 20;
+            myStruct structA = new myStruct();
+            myStruct structB = structA;
+            structA.val = 30;
+            structB.val = 40;
+            Console.WriteLine("ObjectA.val = {0}",objectA.val);
+            Console.WriteLine("ObjectB.val = {0}", objectB.val);
+            Console.WriteLine("structA.val = {0}", structA.val);
+            Console.WriteLine("structB.val = {0}", structB.val);
+        }
+    }
+}
